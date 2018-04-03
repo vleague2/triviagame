0af9fe39aaf2ede1f39edda0e74d03a894322231
unknown <vleague2@gmail.com>
2018-04-03 18:54:37 -0400

    got a question to display and the timer to start!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,16 +6,21 @@
     // Answer option4
     // function: Display "Correct!" when guessed right and increment right counter. wait 3000 and then call startTimer and show next question
     // function: Display "Nope, it was X" when guessed wrong and increment wrong counter. wait 3000 and then call startTimer and show next question
-var quizQuestions = [
-    question1 = {
+var quizQuestions = {
+    question1: {
         question: "What breed of cat is Pusheen?",
         option1: "Domestic shorthair",
         option2: "British shorthair",
         option3: "Russian blue",
         option4: "Maine coon",
-        correct: 1
+        correct: 1,
+        correctAnswer: function () {
+            $("#question-head").text("Correct!");
+            rightCounter ++;
+            setTimeout(startTimer, 3000);
+        }
     },
-    question2 = {
+    question2: {
         question: "What type of coat does Pusheen have?",
         option1: "Tortoiseshell", 
         option2: "Grey bi-color",
@@ -24,7 +29,7 @@ var quizQuestions = [
         correct: 3
     },
 
-    question3 = {
+    question3: {
         question: "What year was Pusheen created?",
         option1: "2014",
         option2: "2010",
@@ -33,7 +38,7 @@ var quizQuestions = [
         correct: 2
     },
 
-    question4 = {
+    question4: {
         question: "What language is Pusheen's name derived from?",
         option1: "Malaysian",
         option2: "Arabic",
@@ -41,25 +46,76 @@ var quizQuestions = [
         option4: "Irish", 
         correct: 4
     }
-]
-
-
+}
 
 // Guessed right counter = 0
+var rightCounter = 0;
 
 // Guessed wrong counter = 0
+var wrongCounter = 0;
 
 // Timer display? starts at 15
+var timer = 15;
+
+// empty variable for us to fill later
+var intervalID;
 
 // function timesUp will 
     // show array.guessedwrong
     // show #select button but change text to Start over?
 
+
+
+// function startTimer will USE SETINTERVAL!!!!
+function startTimer() {
+    timer = 15;
+    $("#time").css('color', 'black');
+    intervalID = setInterval(count, 1000);
+}
+
+// function count will decrement timer and display it
+function count() {
+    timer--;
+    $("#time").text(timer);
+
+    if (timer === 5) {
+        almostUp();
+    }
+
+    if (timer === 0) {
+        stop();
+    }
+}
+
 // function almostUp will change the timer to red
+function almostUp() {
+    $("#time").css('color', 'red');
+}
 
-// function startTimer will USE SETINTERVAL!!!! setTimeout(almostUp, 10000);and setTimeout(timesUp, 15000);
+// function stop will stop the timer at 0
+function stop() {
+    clearInterval(intervalID);
+}
 
-// function clearTimer will clearTimeout(almostUp); clearTimeout(timesUp);
+function nextQuestion() {
+    $("#select").css('display', 'none');
+
+    $("#question-head").text(quizQuestions.question1.question);
+
+    $("#buttons").append("<div><button type='button' class='btn btn-light btn-lg btn-block'>" + quizQuestions.question1.option1 + "</button></div>");
+
+    $("#buttons").append("<div><button type='button' class='btn btn-light btn-lg btn-block'>" + quizQuestions.question1.option2 + "</button></div>");
+
+    $("#buttons").append("<div><button type='button' class='btn btn-light btn-lg btn-block'>"+ quizQuestions.question1.option3 + "</button></div>");
+
+    $("#buttons").append("<div><button type='button' class='btn btn-light btn-lg btn-block'>"+ quizQuestions.question1.option4 + "</button></div>");
+}
+
+$("#select").click(function() {
+    nextQuestion(), startTimer()
+});
+
+
 
 // function startGame will 
     // call the startTimer function
